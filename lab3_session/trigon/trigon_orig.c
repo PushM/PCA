154e845e84f431437d87e7775f53f60aae9004e6
Adur Saizar Ardanuy <adurenator@gmail.com>
2014-03-18 10:45:08 +0100

    trigon

--- lab3_session/trigon/trigon_orig.c
+++ lab3_session/trigon/trigon_orig.c
@@ -8,23 +8,10 @@
 #define N 6000
 #define PUNTS 1000
 
-double **memoization;
-
 int main(int argc, char *argv[])
 {
 	unsigned int i, r, j, n;
 	double d, x, y;
-
-	memoization = malloc(sizeof(double) * 2 * PUNTS);
-
-	// Construct memoization cache
-	for (i = 0, d = 0; i < PUNTS; ++i)
-	{
-		memoization[i][0] = sin(d);
-		memoization[i][1] = cos(d);
-		d += 2*M_PI/PUNTS;
-	}
-	
 
 	if (argc == 1) n = N; else n = atoi(argv[1]);
 
@@ -34,14 +21,10 @@ int main(int argc, char *argv[])
 		r = rand();
 		for (j=0, d=0; j<PUNTS; j++)
 		{
-			/*x = r*cos(d); y = r*sin(d);
+			x = r*cos(d); y = r*sin(d);
 			fwrite(&x, sizeof(x), 1, stdout);
 			fwrite(&y, sizeof(y), 1, stdout);
-			d += 2*M_PI/PUNTS;*/
-			x = r * memoization[j][1];
-			y = r * memoization[j][0];
-			fwrite(&x, sizeof(x), 1, stdout);
-			fwrite(&y, sizeof(y), 1, stdout);
+			d += 2*M_PI/PUNTS;
 		}
 	}
 	return 0;
