9842affb77f31132cee3ae58bc27bc2e47590677
Adur Saizar Ardanuy <adurenator@gmail.com>
2014-03-22 13:04:40 +0100

    lab3 trigon improved

--- lab3_session/trigon/trigon.c
+++ lab3_session/trigon/trigon.c
@@ -8,22 +8,19 @@
 #define N 6000
 #define PUNTS 1000
 
-double *memoization;
-double *buffer;
+double memoization[PUNTS][2];
+double buffer[PUNTS * 2];
 
 int main(int argc, char *argv[])
 {
 	unsigned int i, r, j, n, index;
 	double d;
 
-	memoization = malloc(sizeof(double) * 2 * PUNTS);
-	buffer = malloc(sizeof(double) * 2 * PUNTS);
-
 	// Construct memoization cache
 	for (i = 0, d = 0; i < PUNTS; ++i)
 	{
-		memoization[   i<<1   ] = cos(d);
-		memoization[(i<<1) + 1] = sin(d);
+		memoization[i][0] = cos(d);
+		memoization[i][1] = sin(d);
 		d += 2*M_PI/PUNTS;
 	}
 	
@@ -31,14 +28,13 @@ int main(int argc, char *argv[])
 	else n = atoi(argv[1]);
 
 	srand(0);
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++) {
 		r = rand();
-		for (j = 0; j < PUNTS; j++)
-		{
-			index = j<<1;
-			buffer[  index  ] = r * memoization[  index  ]; // x
-			buffer[index + 1] = r * memoization[index + 1]; // y
+		index = 0;
+		for (j = 0; j < PUNTS; j++) {
+			buffer[  index  ] = r * memoization[j][0]; // x
+			buffer[index + 1] = r * memoization[j][1]; // y
+			index += 2;
 		}
 		fwrite(buffer, sizeof(double), 2 * PUNTS, stdout);
 	}
